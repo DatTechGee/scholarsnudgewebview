d6f238cecbc895a85e09ad7886c4c52f80149503
isaac <ei0413405@gmail.com>
2026-06-15 19:15:25 +0100

    your message

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/school',
+  output: 'export',
+  images: { unoptimized: true },
 }
 module.exports = nextConfig
